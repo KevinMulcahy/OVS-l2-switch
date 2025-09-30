ac2deac7f134ddf9d6332cf657589f6ae0a2a873
KevinMulcahy <kevmul_01@msn.com>
2025-09-30 09:01:37 -0400

    Update vlan.rs

--- fuzz/fuzz_targets/vlan.rs
+++ fuzz/fuzz_targets/vlan.rs
@@ -1,5 +1,9 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
 use dataplane_rs::features::forwarding::types::VlanTag;
 
 fuzz_target!(|data: &[u8]| {
-    let _tag = VlanTag(data.get(0).cloned().unwrap_or(0) as u16);
+    // construct a VLAN tag from the first byte
+    let _tag = VlanTag(data.get(0).copied().unwrap_or(0) as u16);
 });
