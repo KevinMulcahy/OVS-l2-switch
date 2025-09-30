2891a8fc43e4c6bc0096ab79bca9d690e96ea2a8
KevinMulcahy <kevmul_01@msn.com>
2025-09-30 13:18:10 -0400

    Update ethernet.rs

--- fuzz/fuzz_targets/ethernet.rs
+++ fuzz/fuzz_targets/ethernet.rs
@@ -1,7 +1,9 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
+#![no_main]
+
 use dataplane_rs::features::forwarding::types::EthernetFrame;
+use libfuzzer_sys::fuzz_target;
 use std::convert::TryFrom;
 
 fuzz_target!(|data: &[u8]| {
