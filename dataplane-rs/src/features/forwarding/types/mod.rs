0d32a2298fe1e04be7237cf12124d102759924b4
KevinMulcahy <kevmul_01@msn.com>
2025-10-01 13:13:41 -0400

    Update mod.rs

--- dataplane-rs/src/features/forwarding/types/mod.rs
+++ dataplane-rs/src/features/forwarding/types/mod.rs
@@ -4,9 +4,11 @@ pub mod forwarding_entry;
 pub mod mac_address;
 pub mod vlan;
 // pub mod other_type; // add more as needed
+pub mod net_if;
 
 // Re-export for easier access
 pub use forwarding_entry::ForwardingEntry;
 pub use mac_address::MacAddress;
 pub use vlan::Vlan;
 // pub use other_type::OtherType; // add more as needed
+pub use net_if::NetIf;
