f60e44790a5a01c54592eff1023c0fab22eb0ccc
KevinMulcahy <kevmul_01@msn.com>
2025-10-02 13:37:56 -0400

    Update net_if.rs

--- dataplane-rs/src/features/forwarding/types/net_if.rs
+++ dataplane-rs/src/features/forwarding/types/net_if.rs
@@ -1,6 +1,14 @@
-// types/net_if.rs
 #[derive(Debug, Clone)]
 pub struct NetIf {
     pub name: String,
     pub index: u32,
+}
+
+impl NetIf {
+    pub fn new(name: &str, index: u32) -> Self {
+        Self {
+            name: name.to_string(),
+            index,
+        }
+    }
 }
