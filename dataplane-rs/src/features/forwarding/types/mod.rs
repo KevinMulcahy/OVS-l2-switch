73590f7c30ee5e26fb8b81423539f9dd1ac53f65
KevinMulcahy <kevmul_01@msn.com>
2025-10-02 15:43:50 -0400

    Update mod.rs

--- dataplane-rs/src/features/forwarding/types/mod.rs
+++ dataplane-rs/src/features/forwarding/types/mod.rs
@@ -2,8 +2,10 @@ pub mod forwarding_entry;
 pub mod mac_address;
 pub mod net_if;
 pub mod vlan;
+pub mod ethernet_frame;
 
 pub use forwarding_entry::ForwardingEntry;
 pub use mac_address::MacAddress;
 pub use net_if::NetIf;
 pub use vlan::{VlanId, VlanTag}; // ✅ matches your vlan.rs
+pub use ethernet_frame::EthernetFrame;
