b3935fd6d20539bddc3b8853a80c223dd3e4d503
KevinMulcahy <kevmul_01@msn.com>
2025-10-02 15:36:14 -0400

    Update main.rs

--- dataplane-rs/src/main.rs
+++ dataplane-rs/src/main.rs
@@ -1,7 +1,7 @@
 use dataplane_rs::features::forwarding::service::ForwardingService;
 
 fn main() {
-    let svc = ForwardingService::default();
+    let svc = ForwardingService::new();
     let _pipeline = svc
         .setup_pipeline("eth0", "eth1")
         .expect("failed to set up pipeline");
